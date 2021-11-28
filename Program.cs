52a9445588f5574fc91849c15973d08e31f4b12c
Tauseef-Iqbal <Tauseef.Iqbal@coxautoinc.com>
2021-11-28 22:37:40 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AvgWordCountCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
